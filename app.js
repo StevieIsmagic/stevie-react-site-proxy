5691c49678be207ec14ed292a78180b7755ad533
stevieismagic <stevieismagic@gmail.com>
2020-06-08 22:43:32 -0400

    ad idem

--- app.js
+++ app.js
@@ -5,20 +5,29 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.get('*', (req, res) => {
-  request(
-    { url: 'https://nameless-ravine-33561.herokuapp.com/token.js' },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
-      }
-      console.log("PROXY RES \n", body)
-      return body;
-    }
-  )
+  // request(
+  //   { url: 'https://nameless-ravine-33561.herokuapp.com/token.js' },
+  //   (error, response, body) => {
+  //     if (error || response.statusCode !== 200) {
+  //       return res.status(500).json({ type: 'error', message: err.message });
+  //     }
+  //     console.log("PROXY RES \n", body)
+  //     return body;
+  //   }
+  // )
+  const url = "https://nameless-ravine-33561.herokuapp.com/token.js"
+  request(url).pipe(res)
+
+
 });
 
 const PORT = process.env.PORT || 3000;
