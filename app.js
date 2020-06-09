870d158bd741eec0cb349de53d8aa02e97adf7f4
stevieismagic <stevieismagic@gmail.com>
2020-06-08 22:26:43 -0400

    ad idem

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const request = require('request');
+const cors = require('cors');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('https://nameless-ravine-33561.herokuapp.com/token.js', (req, res) => {
   request(
     { url: 'https://nameless-ravine-33561.herokuapp.com/token.js' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
       }
-      body;
+      console.log("PROXY RES \n", body)
+      return body;
     }
   )
 });
