6d74d469c941aea7f1fc2b6da079a17adfd60dc1
stevieismagic <stevieismagic@gmail.com>
2020-07-03 18:42:18 -0400

    add env variables

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const cors = require('cors');
+const env = require('dotenv').config()
 
 const app = express();
-
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -14,18 +12,18 @@ app.use((req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-  // request(
-  //   { url: 'https://nameless-ravine-33561.herokuapp.com/token.js' },
-  //   (error, response, body) => {
-  //     if (error || response.statusCode !== 200) {
-  //       return res.status(500).json({ type: 'error', message: err.message });
-  //     }
-  //     console.log("PROXY RES \n", body)
-  //     return body;
-  //   }
-  // )
-  const url = "https://nameless-ravine-33561.herokuapp.com/token.js"
-  request(url).pipe(res.json())
+  request(
+    { url: process.env.LONG_LIVED_TOKEN_REFRESH_URL },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      console.log("PROXY RES \n", body)
+      return body;
+    }
+  )
+  
+  // request(url).pipe(res.json())
 
 
 });
