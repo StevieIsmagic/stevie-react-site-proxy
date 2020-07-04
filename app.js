3db50a68688b48f85a888d1bca98253be78080ba
stevieismagic <stevieismagic@gmail.com>
2020-07-04 13:58:40 -0400

    fetch user / media data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-const { port, instagramGraphAPI, longLivedToken } = require('./config');
+const { port, instagramGraphAPI, longLivedToken, userDataEndpoint, mediaDataEndpoint } = require('./config');
 
 
 const app = express();
@@ -10,7 +10,6 @@ app.use((req, res, next) => {
   next();
 });
 
-const url = `${instagramGraphAPI}${longLivedToken}`
 
 const daysLeft = body => {
   const secondsInADay = 86400;
@@ -20,15 +19,45 @@ const daysLeft = body => {
   return secondsTillTokenExpires / secondsInADay;
 };
 
+const getUser = token => {
+  const getUserURL = `${userDataEndpoint}${token}`;
+  console.log("USER URL", getUserURL)
+  return request({ url: getUserURL }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.log("USER ERR -", error)
+      return response.status(500).json({ type: 'error', message: error.message });
+    }
+    const parsedBody = JSON.parse(body)
+    console.log('GET USER', parsedBody)
+  })
+}
+
+const getUserMedia = token => {
+  const getMediaURL = `${mediaDataEndpoint}${token}`;
+  console.log("MEDIA URL", getMediaURL)
+  return request({ url: getMediaURL }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.log("MEDIA ERR -", error)
+      return response.status(500).json({ type: 'error', message: error.message });
+    }
+    const parsedBody = JSON.parse(body)
+    console.log('GET MEDIA', parsedBody)
+  })
+}
+
 const refresh60DayToken = res => {
+  const refreshTokenUrl = `${instagramGraphAPI}${longLivedToken}`
   return request(
-    { url },
+    { url: refreshTokenUrl },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         console.log("ERR -", error)
         return response.status(500).json({ type: 'error', message: error.message });
       }
       const parsedBody = JSON.parse(body)
+      const token = parsedBody.access_token;
+      console.log('PARSED BODY',  token)
+      getUserMedia(token)
       return res.status(200).send(`Hi Hi. ${Math.floor(daysLeft(parsedBody))} days till Instagram token expires. Enjoy =)`)
       }
     );
--- config.js
+++ config.js
@@ -4,6 +4,7 @@ dotenv.config();
 module.exports = { 
   port: process.env.PORT,
   instagramGraphAPI: process.env.LONG_LIVED_TOKEN_REFRESH_URL,
-  longLivedToken: process.env.LONG_LIVED_60_DAY_TOKEN
-
+  longLivedToken: process.env.LONG_LIVED_60_DAY_TOKEN,
+  userDataEndpoint: process.env.IG_GRAPH_USER_DATA_ENDPOINT,
+  mediaDataEndpoint: process.env.IG_GRAPH_MEDIA_DATA_ENDPOINT
 }
