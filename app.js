2d97cb85b29e19894d73f67294f3327384ba1468
stevieismagic <stevieismagic@gmail.com>
2020-07-03 20:01:15 -0400

    test

--- app.js
+++ app.js
@@ -13,20 +13,20 @@ app.use((req, res, next) => {
 
 const urls = `${process.env.LONG_LIVED_TOKEN_REFRESH_URL}${process.env.LONG_LIVED_60_DAY_TOKEN}`
 app.get('*', (req, res) => {
-  request(
+  console.log("1 - Req\n", req, 'RES\n', res)
+  return request(
     { url: urls },
     (error, response, body) => {
+      console.log("2 - Res\n", res, 'Body\n', body)
       if (error || response.statusCode !== 200) {
         console.log("ERR -", error)
         return res.status(500).json({ type: 'error', message: error.message });
       }
-      // console.log("PROXY RES \n", body)
+      console.log("PROXY RES \n", body)
 
       return body;
     }
   )
-  
-  // request(url).pipe(res.json())
 
 
 });
