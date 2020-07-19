b874aa44a4bfd25b71f5ec1f2822922e1981604f
stevieismagic <stevieismagic@gmail.com>
2020-07-18 23:46:12 -0400

    notes

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ const {
 // refactor to remove requeste package
 // once media id is returned, as opinions on
 // how to handle 20 individual media(id) requests
+// Promise All ?
+
+/* REFACTOR - Remove deprecated 'request' package
+use ky + bluebird.js
+
+ function getMediaIDS(token: string){
+    return new Promise((resolve, reject) => {
+      request(url, (err, res) => {
+        if(err){
+          return reject(err)
+        }
+        resolve(res)
+      })
+    })
+  }
+
+*/
 const app = express();
 
 app.use((req, res, next) => {
@@ -42,16 +59,17 @@ const getBasicUserData = token => {
 
 const getUserMediaIds = (token) => {
   const getMediaURL = `${mediaDataEndpoint}${token}`;
-  let response = request({ url: getMediaURL }, (error, response, body) => {
+  const response = request({ url: getMediaURL }, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       console.log("Media Ids ERR -", error)
       return response.status(500).json({ type: 'error', message: error.message });
     }
     const parsedBody = JSON.parse(body)
     const mediaIdsArray = parsedBody.data.map(obj => obj.id)
+    console.log('ids', mediaIdsArray)
     return mediaIdsArray
   })
-  console.log("RESSS", response)
+  return response
 }
 
 const getSingleMediaObject = async (id, token) => {
@@ -81,8 +99,8 @@ const refresh60DayToken = res => {
       const parsedBody = JSON.parse(body)
       const token = parsedBody.access_token;
       // return token;
-      let mediaIds = getUserMediaIds(token)
-      console.log("MEDIAIDSSSSSSS \n", mediaIds)
+      let mediaIdsArray = getUserMediaIds(token)
+      console.log("MEDIAIDSSSSSSS \n", mediaIdsArray)
       return res.status(200).send(`Hi Hi. ${Math.floor(daysLeft(parsedBody))} days till Instagram token expires. Enjoy =)`)
       }
     );
